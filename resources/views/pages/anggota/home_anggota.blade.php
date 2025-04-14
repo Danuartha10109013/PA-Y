4ac0524aa749be64adee1c9f05ca0064edc89092
Danuartha10109013 <odanuartha@gmail.com>
2025-04-14 19:53:41 +0700

    bayarpokok

--- resources/views/pages/anggota/home_anggota.blade.php
+++ resources/views/pages/anggota/home_anggota.blade.php
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:901315b422f328c775b429f6b1db39a2dee1ef1449d9d8469a4af44d072d5e48
-size 8240
+oid sha256:5bea1ff704b7dc6687382374b7bf7079aa6ce811cd51b3813a8c659678fc8e90
+size 8209
